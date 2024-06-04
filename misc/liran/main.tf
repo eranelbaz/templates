3f9b8a121894638720bc07150d110d5c3c84e245
Elbaz <eranelbaz97+github@gmail.com>
2024-06-04 15:51:55 +0300

    Create main.tf

---
+++ misc/liran/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" {
+
+}
+
+terraform {
+    after_hook "clear" {
+        commands = ["plan", "apply", "destroy"]
+        execute = ["rm", "-rf", "--", "${get_terragrunt_dir()/.terragrunt-cache"]
+        run_on_error = true
+    }
+}
