90e946ad735f9586af513b9ddd6cfe731b329292
Elbaz <eranelbaz97+github@gmail.com>
2025-02-28 22:42:59 +0200

    Update main.tf

--- misc/null-resource/templates/main.tf
+++ misc/null-resource/templates/main.tf
@@ -7,6 +7,21 @@ terraform {
   }
 }
 
+variable "env0_api_key" {
+  description = "The service account to assign roles to"
+  type        = string
+}
+
+variable "env0_api_secret" {
+  description = "The service account to assign roles to"
+  type        = string
+}
+
+provider "env0" {
+  api_key    = var.env0_api_key
+  api_secret = var.env0_api_secret
+}
+
 variable "service_account" {
   description = "The service account to assign roles to"
   type        = string
