fc10c60681862ad090dc1cc46b2569ca91e20ea8
Elbaz <eranelbaz97+github@gmail.com>
2025-03-03 22:36:25 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,15 +1,6 @@
 resource "null_resource" "null" {
 }
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "elbazd"
 
-    workspaces {
-      name = "templates"
-    }
-  }
-}
 
 resource "null_resource" "null2" {
 }
