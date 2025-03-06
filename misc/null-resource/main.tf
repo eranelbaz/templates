5a024c7f4ee4f609cd9ca20165ff37144029d72a
elbaz <eranelbaz97+github@gmail.com>
2025-03-06 17:26:37 -0600

    add bucket

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,2 +1,23 @@
 resource "null_resource" "null" {
 }
+terraform {
+  required_providers {
+    aws = {
+      source = "opentofu/aws"
+      version = "5.89.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-elbaz"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
