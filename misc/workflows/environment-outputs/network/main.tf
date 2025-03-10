2a8ea1cea1fe14656fae52fd4ff54610815d949f
elbaz <eranelbaz97+github@gmail.com>
2025-03-10 15:11:54 -0600

    remove  aks

--- misc/workflows/environment-outputs/network/main.tf
+++ misc/workflows/environment-outputs/network/main.tf
@@ -8,11 +8,6 @@ output "subnet_ids" {
   }
 }
 
-output "aks_subnet_id" {
-  description = "ID of the subnet to deploy the AKS cluster into."
-  value       = "subnet-12345678"
-}
-
 output "vnet_id" {
   description = "VNet ID."
   value       = "vnet-12345678"
