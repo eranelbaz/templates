aaf1a8968c291a75056298c51687382d13575458
elbaz <eranelbaz97+github@gmail.com>
2025-03-10 15:06:16 -0600

    add eks

---
+++ misc/workflows/environment-outputs/eks/main.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {
+  description = "ID of the vpc to deploy the EKS cluster into."
+}
+
+variable "subnet_ids" {
+  description = "ID of the subnet to deploy the EKS cluster into."
+}
+
