4f118e76e4690bb27f261f95a6b0f91f29b5c86b
elbaz <eranelbaz97+github@gmail.com>
2025-03-10 18:49:27 -0600

    better bucket

--- misc/workflows/environment-outputs/storage/main.tf
+++ misc/workflows/environment-outputs/storage/main.tf
@@ -1,4 +1,13 @@
 output "storage_bucket_name" {
   description = "Name of the storage bucket."
-  value       = "carsBucket"
+  value       = "demo-${random_string.random}-${var.bucket_name}"
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = false
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket"
 }
