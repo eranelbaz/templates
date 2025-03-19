c6dd657de3d1d469ab972c8d1aec45d8dfde404d
Elbaz <eranelbaz97+github@gmail.com>
2025-03-19 16:47:24 +0200

    Update main.tf

--- misc/workflows/environment-outputs/eks/main.tf
+++ misc/workflows/environment-outputs/eks/main.tf
@@ -1,6 +1,9 @@
 variable "vpc_id" {
   description = "ID of the vpc to deploy the EKS cluster into."
 }
+variable "vpc_id" {
+  description = "ID of the vpc to deploy the EKS cluster into."
+}
 
 variable "subnet_ids" {
   description = "ID of the subnet to deploy the EKS cluster into."
