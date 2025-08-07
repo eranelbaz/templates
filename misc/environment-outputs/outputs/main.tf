58684dc57824f5cfa0bfe1fe65aa2e47036908f5
eranelbaz97 <eranelbaz97+github@gmail.com>
2025-08-07 14:45:33 +0300

    more outputs

--- misc/environment-outputs/outputs/main.tf
+++ misc/environment-outputs/outputs/main.tf
@@ -10,3 +10,18 @@ output "sensitive_output" {
   value = "sensitive"
   sensitive = true
 }
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = "vpc-05331f8a7bbafe46d"
+}
+
+output "subnets" {
+  description = "List of subnet IDs"
+  value       = ["subnet-02848bf05f2bb56b2", "subnet-097f2839e9050e61b", "subnet-0ff973a4a5be61835"]
+}
+
+output "allowed_cidr_blocks" {
+  description = "List of allowed CIDR blocks"
+  value       = ["10.36.64.0/26", "10.36.64.64/26", "10.36.64.128/26"]
+}
