411dd2a164dc7f534b85c9169a608ced4a631227
eranelbaz97 <eranelbaz97+github@gmail.com>
2025-08-07 14:49:19 +0300

    add reader

--- misc/environment-outputs/inputs/main.tf
+++ misc/environment-outputs/inputs/main.tf
@@ -5,6 +5,21 @@ variable "image_id" {
   type = string
 }
 
+variable "subnets" {
+  type        = list(string)
+  description = "Private Subnets"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  description = "Allowed CIDRS"
+}
+
 output "abc" {
   value = var.image_id
 }
