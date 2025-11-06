1550efe941fef0fa97b99400780cb5512090c86b
Elbaz <eranelbaz97+github@gmail.com>
2025-11-06 15:13:18 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -13,10 +13,12 @@ terraform {
 
 variable "env0_api_key" {}
 variable "env0_api_secret" {}
+variable "env0_api_endpoint" {}
 
 provider "env0" {
   api_key    = var.env0_api_key
   api_secret = var.env0_api_secret
+  api_endpoint = var.env0_api_endpoint
 }
 
 resource "env0_configuration_variable" "example" {
