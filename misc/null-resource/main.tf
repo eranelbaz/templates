6441dd74e611f1168f9ea8478021c2916e7d40de
Elbaz <eranelbaz97+github@gmail.com>
2025-11-06 14:58:04 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,2 +1,12 @@
 resource "null_resource" "null" {
 }
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "pass" {
+  value = random_password.password
+}
