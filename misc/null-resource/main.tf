26ea6b1525bd7afa7d91c16042b30469a1c3d2a5
Elbaz <eranelbaz97+github@gmail.com>
2025-11-27 13:42:48 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,28 +1,2 @@
 resource "null_resource" "null" {
 }
-
-terraform {
-  required_providers {
-    env0 = {
-      source = "env0/env0"
-    }
-  }
-}
-
-# Configure the env0 provider
-
-variable "env0_api_key" {}
-variable "env0_api_secret" {}
-variable "env0_api_endpoint" {}
-
-provider "env0" {
-  api_key    = var.env0_api_key
-  api_secret = var.env0_api_secret
-  api_endpoint = var.env0_api_endpoint
-}
-
-resource "env0_configuration_variable" "example" {
-  name        = "ENVIRONMENT_VARIABLE_NAME"
-  value       = "example value"
-  description = "Here you can fill description for this variable, note this field have limit of 255 chars"
-}
